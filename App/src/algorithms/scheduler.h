30591c5a69c7d585d57da18d700479e57d10d644
Piyush Singh Bhati <piyush.bhati680@gmail.com>
2025-11-14 16:20:13 +0530

    scheduler.h structured

--- App/src/algorithms/scheduler.h
+++ App/src/algorithms/scheduler.h
@@ -4,24 +4,21 @@
 #include <vector>
 #include <tuple>
 
-// Process structure to hold process information
 struct Process {
-    int pid; // Process ID
-    int arrival; // Arrival time
-    int burst; // Burst time
-    int completion; // Completion time
-    int turnaround; // Turnaround time
-    int waiting; // Waiting time
+    int pid;
+    int arrival;
+    int burst;
+    int completion;
+    int turnaround;
+    int waiting;
 };
 
-// Gantt chart entry structure
 struct GanttEntry {
-    int pid; // Process ID
-    int start; // Start time
-    int end; // End time
+    int pid;
+    int start;
+    int end;
 };
 
-// Function declarations for scheduling algorithms
 void fcfs(std::vector<Process> processes);
 void rr(std::vector<Process> processes, int quantum);
 void hrrn(std::vector<Process> processes);
@@ -29,8 +26,7 @@ void mlfq(std::vector<Process> processes);
 void sjf(std::vector<Process> processes);
 void srt(std::vector<Process> processes);
 
-// Utility functions
 void print_result(const std::vector<Process>& processes);
 void print_gantt(const std::vector<GanttEntry>& chart);
 
-#endif // SCHEDULER_H
+#endif
